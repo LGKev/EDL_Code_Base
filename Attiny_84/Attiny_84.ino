9f1360ac14bdd7f1116214246b1c2570a838f3c3
Kkuwata_imac <kevinkuwata@icloud.com>
2018-04-16 23:01:47 -0600

    fixing file format

---
+++ Attiny_84/Attiny_84.ino
@@ -0,0 +1,47 @@
+/*
+	code for the adc on the attiny84
+	Author: Kevin Kuwata
+	https://github.com/LGKev/EDL_Code_Base
+	Created: 4/16/18 10:42pm
+	
+	uses ATTinyCore by Spence Konde
+	https://github.com/SpenceKonde/ATTinyCore
+	
+	
+	This MCU will act as a slave. It has a default address of 0x28 and will respond accordinginly. 
+	Plan is to have the commands to read an ADC and then report back the value over i2c when the MASTER 
+	asks for a given amount of data. I envision the following commands:
+		
+		Pin				pin number sent from MASTER
+		ReadADC			MASTER asks for 1 byte and the slave returns the value
+		AnalogWrite 	Takes a pin and sets the value
+		
+		MCU_Report		the Slave will respond with data from the line follower and a byte on what the final command is. 
+						ie turn left, turn right, straight
+						
+						
+	FOR now. to prove the ADC work and the pwm works we are going to simply read the ADC of the first few and then write the values 
+	to pwm to show that the duty cycles are indead different!
+*/
+
+#include <Wire.h>
+
+#define DEFAULT_ADDRESS_ATTINY84 0X28
+
+void setup(){
+	analogWrite(A7, 100); // measure on pcb a duty cycle that is 100/255 
+}
+
+int analogA0 = 0;
+int analogA1 = 0;
+void loop(){
+	analogA0 = analogRead(A0);
+	analogA1 = analogRead(A1);
+	
+	
+	
+	analogWrite(A2, analogA0);
+	analogWrite(A1, analogA1);
+	
+	
+}	
